7227c8d0cfe073bab39afe92366ed57f0550b1dc
pancake <pancake@nopcode.org>
2025-11-23 14:05:27 +0100

    Reference the html

--- gen.sh
+++ gen.sh
@@ -2,5 +2,5 @@
 A=$1
 mkdir -p www
 cat head.html > www/"$A".html
-pandoc --highlight-style=zenburn < src/"$A".md >> www/"$A".html || exit 1
+pandoc --syntax-highlighting=zenburn < src/"$A".md >> www/"$A".html || exit 1
 cat tail.html >> www/"$A".html
--- src/index.md
+++ src/index.md
@@ -2,7 +2,7 @@
 
 ## Reversing
 
-* [Fixing a radare2 bug on the Windows Terminal](./WinCdbNull.md)
+* [Fixing a radare2 bug on the Windows Terminal](./WinCdbNull.html)
 
 ## AI/LLM
 
